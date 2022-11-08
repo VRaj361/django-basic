0829ece97486769693bf0450e111e96b9f692c28
Vraj <vrajnpatel16@gmail.com>
2022-11-08 20:52:52 +0530

    Model Information to create Database table

--- learning/blog/models.py
+++ learning/blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+#using command python3 manage.py makemigrations and python3 manage.py migrate
+# sourse venv/bin/activate
 # Create your models here.
+class Employee(models.Model):
+
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    age = models.IntegerField()
+    isActivate = models.BooleanField(default=True)
+    password = models.CharField(max_length=20)
+    address = models.TextField()
+    salary = models.FloatField(null=True)
+    mobile = models.BigIntegerField(null=True)
+    createdAt = models.DateTimeField(auto_now_add=True,null=True) 
+    # auto_now_add is for creation time
+    updateedAt = models.DateTimeField(auto_now=True,null=True)
+    # auto now is for updation time
+    birthdate = models.DateField(null=True)
+    birthtime = models.TimeField(null=True)
+    # table name
+    class Meta:
+        db_table = "employees"
+
+    # present data on admin side with name not object
+    def __str__(self):
+        return self.name 
